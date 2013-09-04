a6a61b6354e84147e9c84971e465b88a1b8fd92c
Cameron Garrett <Cameron913@gmail.com>
2013-09-04 00:38:14 -0500

    slider semi-working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
 // allow :active styles to work on iOS & Android
 document.addEventListener("touchstart", function(){}, true);
+
+// iDangerous Swiper init
+window.onload = function() {
+  var mySwiper = new Swiper('.swiper-container',{
+    //Your options here:
+    mode:'horizontal',
+    loop: true,
+    calculateHeight: true,
+    grabCursor: true,
+	    updateOnImagesReady: true,
+	    autoResize: true,
+	    simulateTouch: true
+  });
+}
