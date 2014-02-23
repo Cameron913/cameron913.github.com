f0457587f7e1e711f14c14d4f82ef2bafe1c74b4
Cameron Garrett <Cameron913@gmail.com>
2014-02-22 22:35:23 -0600

    reformat swiper init js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,14 @@ document.addEventListener("touchstart", function(){}, true);
 // iDangerous Swiper init
 window.onload = function() {
   var mySwiper = new Swiper('.swiper-container',{
-    //Your options here:
     mode:'horizontal',
     loop: true,
     calculateHeight: true,
     grabCursor: true,
     keyboardControl: true,
     autoplay: 5000,
-	    updateOnImagesReady: true,
-	    autoResize: true,
-	    simulateTouch: true
+    updateOnImagesReady: true,
+    autoResize: true,
+    simulateTouch: true
   });
 }
